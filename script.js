51c7938d89ea156ca846430fa74df290379dfd6f
marslikbola <mengboyevfarrux5@gmail.com>
2024-11-15 15:00:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").addEventListener("click", ()=>{
+    document.querySelector("ul").style.display = "block"
+})
+
+document.getElementById("close").addEventListener("click", ()=>{
+    document.querySelector("ul").style.display = "none"
+})
